4d9310a0dda0eec9af803f36703b7ae77b92c95e
Antropov Nikita <nikitaantropov@inbox.ru>
2022-12-22 19:49:14 +0300

    Add lab 1005

---
+++ Lab1005/lab1005.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <stdint.h>
+#include <array>
+
+#define MAX_N 20 // Максимальное кол-во камней
+
+using namespace std;
+
+/**
+ * @brief Расчет минимальной вохможной разницы веса двух кучек камней методом перебора
+ * @param w     Массив весов камней
+ * @param n     Кол-во камней
+ * @param w_tot Общий вес камней
+ * @param w_cur Текущий вес первой кучки
+ * @return Минимальная разница веса двух кучек
+*/
+uint32_t min_diff(array<uint32_t, MAX_N>& w, size_t n, const uint32_t w_tot, uint32_t w_cur);
+
+int main()
+{
+    size_t n = 0; // Кол-во камней
+    cin >> n;
+
+    array<uint32_t, MAX_N> w = {0};
+    uint32_t w_total = 0; // Общий вес камней
+
+    for(size_t i=0; i < n; i++)
+    {
+        cin >> w[i];
+        w_total += w[i];
+    }
+
+    cout << min_diff(w, n, w_total, 0);
+
+    return 0;
+}
+
+uint32_t min_diff(array<uint32_t, MAX_N>& w, size_t n, const uint32_t w_tot, uint32_t w_cur)
+{
+    if (n == 0 || w_cur >= w_tot/2) // Тривиальный исход - закончились камни или вес текущей кучки превысил половину общего веса
+    {
+        int32_t diff = w_tot - 2*w_cur;
+        return abs(diff);
+    }
+    n--;
+    return min(min_diff(w, n, w_tot, w_cur + w[n]), min_diff(w, n, w_tot, w_cur)); // Выбор минимального значения из кучки с текущим камнем и без него 
+}
